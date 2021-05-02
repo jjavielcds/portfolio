7bec545da6039896488b9964c827c3319fe01e1b
jjaviel <jjaviel@codigodelsur.com>
2021-05-01 22:47:25 -0300

    add annotate gem

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: investments
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  tir           :decimal(10, 5)
+#  kind          :integer          not null
+#  user_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  start_date    :date
+#  end_date      :date
+#  initial_value :decimal(10, 5)
+#  final_value   :decimal(10, 5)
+#  coin          :integer
+#
 class Investment < ApplicationRecord
   belongs_to :user
   enum kind: { cheque: 0, other: 1 }
@@ -11,4 +28,9 @@ class Investment < ApplicationRecord
             :initial_value,
             :coin,
             presence: true
+
+  private
+  
+  def set_tir
+  end
 end
