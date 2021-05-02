51d84c743cd377fec4e432cf613c6e65e4ccc09e
jjavielcds <52083120+jjavielcds@users.noreply.github.com>
2021-05-01 22:27:49 -0300

    Cleaning codebase (#3)
    * refactor to clean code
    
    * fix botstrap
    
    * add specs

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,4 +4,11 @@ class Investment < ApplicationRecord
   belongs_to :user
   enum kind: { cheque: 0, other: 1 }
   enum coin: { peso: 0, dolar: 1 }
+
+  validates :name,
+            :kind,
+            :start_date,
+            :initial_value,
+            :coin,
+            presence: true
 end
