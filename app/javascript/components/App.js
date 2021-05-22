7e0b851f96b5028aa3d0b8bf5ba7c4202602b25e
jjaviel <jjaviel@codigodelsur.com>
2021-05-22 20:55:17 -0300

    add react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+    render(){
+        const hello = 'Hello world!'
+        return(
+            <div>{hello}</div>
+        )
+    }
+}
+
+export default App
